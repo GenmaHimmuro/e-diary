5b522fff437bbdae538e59edf9ba96ff8c9d142a
GenmaHimmuro <t.suleimanov2@gmail.com>
2025-03-09 14:57:30 +0500

    Скрипты для электронного дневника

---
+++ scripts.py
@@ -0,0 +1,40 @@
+from datacenter.models import Schoolkid
+from datacenter.models import Mark
+from datacenter.models import Chastisement
+from datacenter.models import Commendation
+from datacenter.models import Lesson
+from random import choice
+
+
+def find_person(name, year_of_study, group_letter):
+    try:
+        return Schoolkid.objects.get(full_name__contains=name, year_of_study=year_of_study,
+                                     group_letter=group_letter)
+    except Schoolkid.DoesNotExist:
+        raise Schoolkid.DoesNotExist("Ученик не найден!") from None
+    except Schoolkid.MultipleObjectsReturned:
+        raise Schoolkid.MultipleObjectsReturned("Найдено несколько учеников!") from None
+
+
+def fix_marks(name, year_of_study, group_letter):
+    schoolkid = find_person(name, year_of_study, group_letter)
+    Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3]).update(points=choice([4, 5]))
+
+
+def remove_chastisements(name, year_of_study, group_letter):
+    schoolkid = find_person(name, year_of_study, group_letter)
+    Chastisement.objects.filter(schoolkid=schoolkid).delete()
+
+
+def create_commendation(name, year_of_study, group_letter, lesson):
+    schoolkid = find_person(name, year_of_study, group_letter)
+
+    lesson = Lesson.objects.filter(year_of_study=schoolkid.year_of_study, group_letter=schoolkid.group_letter,
+                                   subject__title=lesson).order_by("?").first()
+    if lesson is None:
+        raise Lesson.DoesNotExist("Предмет не найден!")
+    with open("Похвалы.txt", "r", encoding="utf8") as file:
+        messages = file.readlines()
+        massage = choice(messages)
+        Commendation.objects.create(teacher=lesson.teacher, subject=lesson.subject, schoolkid=schoolkid,
+                                    created=lesson.date, text=massage)
