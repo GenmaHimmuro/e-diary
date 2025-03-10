fb491d271c7e34eb7ea0919bf84d12b3aa69784e
GenmaHimmuro <t.suleimanov2@gmail.com>
2025-03-10 17:17:05 +0500

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -7,34 +7,48 @@ from random import choice
 
 
 def find_person(name, year_of_study, group_letter):
-    try:
         return Schoolkid.objects.get(full_name__contains=name, year_of_study=year_of_study,
                                      group_letter=group_letter)
-    except Schoolkid.DoesNotExist:
-        raise Schoolkid.DoesNotExist("Ученик не найден!") from None
-    except Schoolkid.MultipleObjectsReturned:
-        raise Schoolkid.MultipleObjectsReturned("Найдено несколько учеников!") from None
 
 
 def fix_marks(name, year_of_study, group_letter):
-    schoolkid = find_person(name, year_of_study, group_letter)
+    try:
+        schoolkid = find_person(name, year_of_study, group_letter)
+    except Schoolkid.DoesNotExist:
+        raise Schoolkid.DoesNotExist("Ученик не найден!")
+    except Schoolkid.MultipleObjectsReturned:
+        raise Schoolkid.MultipleObjectsReturned("Найдено несколько учеников!")
     Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3]).update(points=choice([4, 5]))
 
 
 def remove_chastisements(name, year_of_study, group_letter):
-    schoolkid = find_person(name, year_of_study, group_letter)
+    try:
+        schoolkid = find_person(name, year_of_study, group_letter)
+    except Schoolkid.DoesNotExist:
+        raise Schoolkid.DoesNotExist("Ученик не найден!")
+    except Schoolkid.MultipleObjectsReturned:
+        raise Schoolkid.MultipleObjectsReturned("Найдено несколько учеников!")
     Chastisement.objects.filter(schoolkid=schoolkid).delete()
 
 
 def create_commendation(name, year_of_study, group_letter, lesson):
-    schoolkid = find_person(name, year_of_study, group_letter)
+    try:
+        schoolkid = find_person(name, year_of_study, group_letter)
+    except Schoolkid.DoesNotExist:
+        raise Schoolkid.DoesNotExist("Ученик не найден!")
+    except Schoolkid.MultipleObjectsReturned:
+        raise Schoolkid.MultipleObjectsReturned("Найдено несколько учеников!")
 
     lesson = Lesson.objects.filter(year_of_study=schoolkid.year_of_study, group_letter=schoolkid.group_letter,
                                    subject__title=lesson).order_by("?").first()
-    if lesson is None:
+    if not lesson:
         raise Lesson.DoesNotExist("Предмет не найден!")
-    with open("Похвалы.txt", "r", encoding="utf8") as file:
-        messages = file.readlines()
-        massage = choice(messages)
-        Commendation.objects.create(teacher=lesson.teacher, subject=lesson.subject, schoolkid=schoolkid,
+
+    try:
+        with open("Похвалы.txt", "r", encoding="utf8") as file:
+            messages = file.readlines()
+            massage = choice(messages)
+            Commendation.objects.create(teacher=lesson.teacher, subject=lesson.subject, schoolkid=schoolkid,
                                     created=lesson.date, text=massage)
+    except (FileExistsError, IndexError):
+        print('Файл не найден или пуст')
