0adf2b1e356afae908078564a87c3383bd7c8e63
GenmaHimmuro <t.suleimanov2@gmail.com>
2025-03-13 11:32:35 +0500

    Исправления

--- scripts.py
+++ scripts.py
@@ -7,48 +7,37 @@ from random import choice
 
 
 def find_person(name, year_of_study, group_letter):
+    try:
         return Schoolkid.objects.get(full_name__contains=name, year_of_study=year_of_study,
                                      group_letter=group_letter)
+    except Schoolkid.DoesNotExist:
+        raise Schoolkid.DoesNotExist("Ученик не найден!") from None
+    except Schoolkid.MultipleObjectsReturned:
+        raise Schoolkid.MultipleObjectsReturned("Найдено несколько учеников!") from None
 
 
 def fix_marks(name, year_of_study, group_letter):
-    try:
-        schoolkid = find_person(name, year_of_study, group_letter)
-    except Schoolkid.DoesNotExist:
-        raise Schoolkid.DoesNotExist("Ученик не найден!")
-    except Schoolkid.MultipleObjectsReturned:
-        raise Schoolkid.MultipleObjectsReturned("Найдено несколько учеников!")
+    schoolkid = find_person(name, year_of_study, group_letter)
     Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3]).update(points=choice([4, 5]))
 
 
 def remove_chastisements(name, year_of_study, group_letter):
-    try:
-        schoolkid = find_person(name, year_of_study, group_letter)
-    except Schoolkid.DoesNotExist:
-        raise Schoolkid.DoesNotExist("Ученик не найден!")
-    except Schoolkid.MultipleObjectsReturned:
-        raise Schoolkid.MultipleObjectsReturned("Найдено несколько учеников!")
+    schoolkid = find_person(name, year_of_study, group_letter)
     Chastisement.objects.filter(schoolkid=schoolkid).delete()
 
 
 def create_commendation(name, year_of_study, group_letter, lesson):
-    try:
-        schoolkid = find_person(name, year_of_study, group_letter)
-    except Schoolkid.DoesNotExist:
-        raise Schoolkid.DoesNotExist("Ученик не найден!")
-    except Schoolkid.MultipleObjectsReturned:
-        raise Schoolkid.MultipleObjectsReturned("Найдено несколько учеников!")
+    schoolkid = find_person(name, year_of_study, group_letter)
 
     lesson = Lesson.objects.filter(year_of_study=schoolkid.year_of_study, group_letter=schoolkid.group_letter,
                                    subject__title=lesson).order_by("?").first()
     if not lesson:
         raise Lesson.DoesNotExist("Предмет не найден!")
-
     try:
         with open("Похвалы.txt", "r", encoding="utf8") as file:
             messages = file.readlines()
             massage = choice(messages)
             Commendation.objects.create(teacher=lesson.teacher, subject=lesson.subject, schoolkid=schoolkid,
                                     created=lesson.date, text=massage)
-    except (FileExistsError, IndexError):
-        print('Файл не найден или пуст')
+    except FileExistsError:
+        print('Файл не найден')
